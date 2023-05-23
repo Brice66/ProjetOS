665e9177788f5de08cd5d81c388038f466bacd2c
Ichiky_Otsu <66952647+Brice66@users.noreply.github.com>
2023-05-23 20:10:54 +0200

    Update V5FINALE.c

--- V5FINALE.c
+++ V5FINALE.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
 
     // Boucle pour exécuter chaque fichier shell
     for (int i = 1; i < argc; i++) {
-
+	printf("j'ouvre le fichier\n");
         // Ouvrir le fichier shell
         FILE* shell_file = fopen(argv[i], "r");
         if (!shell_file) {
@@ -49,7 +49,7 @@ int main(int argc, char *argv[]) {
                 exit(1);
             } else if (pid == 0) {
                 // Nous sommes dans le processus fils
-
+		printf("Bonjour, je suis le processus fils\n");
                 // Fermer l'extrémité de lecture du canal
                 close(pipefd[0]);
 
@@ -57,15 +57,15 @@ int main(int argc, char *argv[]) {
                 dup2(pipefd[1], STDOUT_FILENO);
 
                 // Exécuter la commande
-               // execl("/bin/sh", "sh", "-c", buffer, (char *) NULL);
+                //execl("/!bin/sh", "sh", "-c", buffer, (char *) NULL);
                 char *args[] = {"sh", "-c", buffer, NULL};
                 execvp(args[0], args);
-                //Si execl renvoie, cela signifie qu'il y a eu une erreur
+                // Si exevp renvoie, cela signifie qu'il y a eu une erreur
                 printf("Erreur lors de l'exécution de la commande.\n");
                 exit(1);
             } else {
                 // Nous sommes dans le processus père
-
+		printf("Bonjour, je suis le processus père\n");
                 // Fermer l'extrémité d'écriture du canal
                 close(pipefd[1]);
 
@@ -75,6 +75,7 @@ int main(int argc, char *argv[]) {
     		int total_bytes = 0;
                 while ((nbytes = read(pipefd[0], result + total_bytes, BUFFER_SIZE - total_bytes)) > 0) {
                     total_bytes += nbytes;
+                    printf("Lecture en cours\n");
                 }
 
                 // Fermer l'extrémité de lecture du canal
@@ -90,13 +91,15 @@ int main(int argc, char *argv[]) {
                 } else {
                     printf("Erreur lors de la lecture du résultat depuis le canal.\n");
                 }
-                printf("Le processus fils a terminé avec le code de retour %d.\n", WEXITSTATUS(status));
+                printf("Le processus fils a terminé avec le code de retour %d.\n\n\n", WEXITSTATUS(status));
+                // condition qui arrète le programme en cas d'erreurs (choix personnel)
+                if(WEXITSTATUS(status)!=0){exit(1);}
             }
 
-    }
+    	}	
     
-    // Fermer le fichier shell
-    fclose(shell_file);
+	    // Fermer le fichier shell
+	    fclose(shell_file);
 }
     return 0;
 }
